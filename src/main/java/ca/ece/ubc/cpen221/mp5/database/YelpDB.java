88178b9d949a9c631a62ea12da7a0a365be566b8
jeremywiens <jeremydwiens@gmail.com>
2017-11-25 13:29:47 -0800

    Will it work?

---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/database/YelpDB.java
@@ -0,0 +1,43 @@
+package ca.ece.ubc.cpen221.mp5.database;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.ToDoubleBiFunction;
+
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+
+public class YelpDB implements MP5Db {
+
+	private List<Restaurants> restaurants;
+	private List<Reviews> reviews;
+	private List<Users> users;
+
+	// Maybe we should change the order so users are created first? Then every new
+	// review object can be added to the user who wrote it?
+	public YelpDB(String restaurants_file, String reviews_file, String users_file) throws IOException {
+		restaurants = ParseJSON.ParseFile(restaurants_file, 1);
+		reviews = ParseJSON.ParseFile(reviews_file, 2);
+		users = ParseJSON.ParseFile(users_file, 3);
+	}
+
+	@Override
+	public Set getMatches(String queryString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String kMeansClusters_json(int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ToDoubleBiFunction getPredictorFunction(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
