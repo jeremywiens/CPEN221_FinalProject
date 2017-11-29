8974fa35b31acc766886b57adc4636f2a4e5bf43
jeremywiens <jeremydwiens@gmail.com>
2017-11-29 12:04:52 -0800

    Why is this happenning

--- src/main/java/ca/ece/ubc/cpen221/mp5/database/Restaurant.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/database/Restaurant.java
@@ -1,4 +1,5 @@
 package ca.ece.ubc.cpen221.mp5.database;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,101 +14,117 @@ import javax.json.stream.JsonParser;
 import javax.json.stream.JsonParser.Event;
 import javax.json.stream.JsonParserFactory;
 
-public class Restaurant /*extends Product*/{
+public class Restaurant /* extends Product */ {
 
-	private int review_count;
-	private String photo_url;
-	private String state;
-	private String full_address;
-	private String type;
-	private String url;
-	private String city;
+	private int review_count = 0;
+	private String photo_url = null;
+	private String state = null;
+	private String full_address = null;
+	private String type = null;
+	private String url = null;
+	private String city = null;
 	private Boolean open;
 	private ArrayList<String> neighborhoods = new ArrayList<String>();
 	private int price;
 	private ArrayList<String> schools = new ArrayList<String>();
-	private BigDecimal stars;
-	private String name;
-	private String business_id;
-	private BigDecimal longitude;
+	private int stars = 0;
+	private String name = null;
+	private String business_id = null;
+	private BigDecimal longitude = null;
 	private List<String> categories = new ArrayList<String>();
-	private BigDecimal latitude;
+	private BigDecimal latitude = null;
 
 	public Restaurant(String Restaurant) {
 		JsonParser parser = Json.createParser(new StringReader(Restaurant));
-	    String key = null;
-	    String value = null;
-	    while (parser.hasNext()) {
-	         final Event event = parser.next();
-	         switch (event) {
-	            case KEY_NAME:
-	                 key = parser.getString();
-	                 break;
-	            case VALUE_STRING:
-	            	if (key.equals("url")) {
-	            		this.url = parser.getString();
-	            	}
-	            	if (key.equals("neighborhoods")) {
-	            		this.neighborhoods.add(parser.getString());
-	            	}
-	            	if (key.equals("business_id")) {
-	            		this.business_id = parser.getString();
-	            	}
-	            	if (key.equals("name")) {
-	            		this.name = parser.getString();
-	            	}
-	            	if (key.equals("categories")) {
-	            		this.categories.add(parser.getString());
-	            	}
-	            	if (key.equals("State")) {
-	            		this.state = parser.getString();
-	            	}
-	            	if (key.equals("type")) {
-	            		this.type = parser.getString();
-	            	}
-	            	if (key.equals("city")) {
-	            		this.city = parser.getString();
-	            	}
-	            	if (key.equals("full_address")) {
-	            		this.full_address = parser.getString();
-	            	}
-	            	if (key.equals("photo_url")) {
-	            		this.photo_url = parser.getString();
-	            	}
-	            	if (key.equals("schools")) {
-	            		this.schools.add(parser.getString());
-	            	}
-	                 break;
-	            case VALUE_NUMBER:
-	            	if (key.equals("price")) {
-	            		this.price = parser.getInt();
-	            	}
-	            	if (key.equals("latitude")) {
-	            		this.latitude = parser.getBigDecimal();
-	            	}
-	            	if (key.equals("review_count")) {
-	            		this.review_count = parser.getInt();
-	            	}
-	            	if (key.equals("stars")) {
-	            		this.stars = parser.getBigDecimal();
-	            	}
-	            	if (key.equals("longitude")) {
-	            		this.longitude = parser.getBigDecimal();
-	            	}
-	                break;
-	            case VALUE_TRUE:
-	            	if (key.equals("open")) {
-	            		this.open = (true);
-	            	}
-	                break;
-	            case VALUE_FALSE:
-	            	if (key.equals("open")) {
-	            		this.open = (false);
-	            	}
-	                break;
-	            }
-	        }
-	        parser.close();
+		String key = null;
+		while (parser.hasNext()) {
+			final Event event = parser.next();
+			switch (event) {
+			case KEY_NAME:
+				key = parser.getString();
+				break;
+			case VALUE_STRING:
+				if (key.equals("url")) {
+					this.url = parser.getString();
+				}
+				if (key.equals("neighborhoods")) {
+					this.neighborhoods.add(parser.getString());
+				}
+				if (key.equals("business_id")) {
+					this.business_id = parser.getString();
+				}
+				if (key.equals("name")) {
+					this.name = parser.getString();
+				}
+				if (key.equals("categories")) {
+					this.categories.add(parser.getString());
+				}
+				if (key.equals("state")) {
+					this.state = parser.getString();
+				}
+				if (key.equals("type")) {
+					this.type = parser.getString();
+				}
+				if (key.equals("city")) {
+					this.city = parser.getString();
+				}
+				if (key.equals("full_address")) {
+					this.full_address = parser.getString();
+				}
+				if (key.equals("photo_url")) {
+					this.photo_url = parser.getString();
+				}
+				if (key.equals("schools")) {
+					this.schools.add(parser.getString());
+				}
+				break;
+			case VALUE_NUMBER:
+				if (key.equals("price")) {
+					this.price = parser.getInt();
+				}
+				if (key.equals("latitude")) {
+					this.latitude = parser.getBigDecimal();
+				}
+				if (key.equals("review_count")) {
+					this.review_count = parser.getInt();
+				}
+				if (key.equals("stars")) {
+					this.stars = parser.getInt();
+				}
+				if (key.equals("longitude")) {
+					this.longitude = parser.getBigDecimal();
+				}
+				break;
+			case VALUE_TRUE:
+				if (key.equals("open")) {
+					this.open = (true);
+				}
+				break;
+			case VALUE_FALSE:
+				if (key.equals("open")) {
+					this.open = (false);
+				}
+				break;
+			}
+		}
+		parser.close();
+		// System.out.println(business_id + categories + city + full_address);
+		if (notNull(this.business_id, this.categories, this.city, this.full_address, this.latitude, this.longitude,
+				this.name, this.neighborhoods, this.categories, this.schools, this.url, this.type, this.photo_url,
+				this.state)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private boolean notNull(Object... args) {
+		for (Object arg : args) {
+			if (arg != null) {
+				return false;
+			} else {
+				System.out.println("hello");
+			}
+		}
+		return true;
 	}
 
 	public Boolean getOpen() {
@@ -150,7 +167,7 @@ public class Restaurant /*extends Product*/{
 		return this.type;
 	}
 
-	public BigDecimal getStars() {
+	public int getStars() {
 		return this.stars;
 	}
 
@@ -169,11 +186,11 @@ public class Restaurant /*extends Product*/{
 	public String getPhotoURL() {
 		return this.photo_url;
 	}
-	
+
 	public int getPrice() {
 		return this.price;
 	}
-	
+
 	public List<String> getSchools() {
 		return this.schools;
 	}
